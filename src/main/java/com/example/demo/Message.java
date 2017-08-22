c7bad6edfd16fcf2055098c3295dc043569f6e94
Afeeni Phillips <afeeniphillips@Afeenis-MacBook-Pro-3.local>
2017-08-21 23:38:31 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @NotNull
+    @Size(min=4)
+    private String user;
+
+    @NotNull
+    @Size(min=10)
+    private String description;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
+
